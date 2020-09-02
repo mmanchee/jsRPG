4dfa7797ac219e35ff826723d152b5cd60df6f64
Mike Manchee <mikemanchee@gmail.com>
2020-09-02 13:38:28 -0700

    add stats class, Test 4 passed
    co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com>

--- __test__/stats.test.js
+++ __test__/stats.test.js
@@ -3,6 +3,11 @@ import { Stats } from './../src/js/stats';
 describe('Stats', () => {
 
   test('should correctly create a stats object from the constructor', () => {
-    const stats = new Stats()
+    const stats = new Stats(1, 2, 3, 4, 5);
+    expect(stats.strength).toEqual(1);
+    expect(stats.intellect).toEqual(2);
+    expect(stats.charm).toEqual(3);
+    expect(stats.wisdom).toEqual(4);
+    expect(stats.power).toEqual(5);
   })
 }) 
